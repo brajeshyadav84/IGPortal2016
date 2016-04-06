1941fd2a36347ffa52972506dd956ef26d8d84bc
Brajesh <brajesh.mymailbox@gmail.com>
2016-04-07 00:19:14 +0800

    Chat Room screen updated

--- app/js/configs/ChatRoomConstant.js
+++ app/js/configs/ChatRoomConstant.js
@@ -1,5 +1,17 @@
 var ChatRoomConstant = {
-    
+    REACTJS : 'https://gitter.im/brajeshyadav84/ReactJS-ChatRoom',
+    ANGULARJS: 'https://gitter.im/brajeshyadav84/AngularJS-ChatRoom',
+    DOTNET: 'https://gitter.im/brajeshyadav84/DotNet-ChatRoom',
+    MVC: 'https://gitter.im/brajeshyadav84/MVC-ChatRoom',
+    JAVASCRIPT:'https://gitter.im/brajeshyadav84/Javascript-ChatRoom',
+    CSS_SASS_LESS: 'https://gitter.im/brajeshyadav84/CSS-ChatRoom',
+    JAVA: 'https://gitter.im/brajeshyadav84/Java-ChatRoom',
+    NODEJS: 'https://gitter.im/brajeshyadav84/NodeJS-ChatRoom',
+    JOB_REFERENCE: 'https://gitter.im/brajeshyadav84/JobReference-ChatRoom',
+    HR_PREPRATION: 'https://gitter.im/brajeshyadav84/HrPrepration-ChatRoom',
+    TESTING: 'https://gitter.im/brajeshyadav84/Testing-ChatRoom',
+    ANDROID:'https://gitter.im/brajeshyadav84/Android-ChatRoom',
+    IOS:'https://gitter.im/brajeshyadav84/IOS-ChatRoom'
 };
 
 module.exports = ChatRoomConstant;
